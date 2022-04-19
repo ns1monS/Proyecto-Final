89f086e5c519ef7a852401c75c5bbf8b734d5135
nsimon <noelsimonaguilarj@gmail.com>
2022-04-19 18:05:39 +0200

    index agregando peticiones

--- Proyecto-Final-Releevant/Cliente/servidor/index.js
+++ Proyecto-Final-Releevant/Cliente/servidor/index.js
@@ -4,46 +4,50 @@ const path = require("path");
 const { MongoClient } = require("mongodb");
 const client = new MongoClient("mongodb://127.0.0.1:27017");
 
-async function conectar(){
-    await client.connect();
-}
-function logError(e) {
-    console.log(e)
-}
-
-function find(baseDatos, collection , filtro, execute , callback , error) {
-    client  
-        .db(baseDatos)
-        .collection(collection)
-        .find(filtro)
-        .forEach(execute)
-        .then(callback)
-        .catch(error)
-}
-
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.json())
-
-app.get("/Lenguaje" , function (req, res){
-    conectar();
-    let posts = [];
-    find(
-        "Proyecto-Final-Releevant",
-        "post",
-        {Lenguaje: { $in: ["JS"] }},
-        (Post) =>{
-        console.log("prueba 1: ", Post.Lenguaje , Post.Usuario );
-        posts.push(Post.Lenguaje , Post.Usuario);
-        },
-        function () {
-            res.send(posts);
-
-        },
-        logError
-    );
-})
+app.get("/names", function (request, response) {
+    response.set("Access-Control-Allow-Origin", "*");
+    MongoClient.connect("mongodb://localhost:27017/", (err, client) => {
+      if (err) throw err;
+  
+      let database = client.db("Proyecto-Final-Releevant");
+  
+      database
+        .collection("usuarios")
+        .find()
+        .toArray((err, results) => {
+          if (err) throw err;
+  
+          results.forEach((value) => {
+            console.log(value.username);
+          });
+          response.json(results);
+        });
+    });
+  });
 
 
+  //  Sacar todos post con Lenguaje JS
+app.get("/busca/:Lenguaje/JS", function (request, response) {
+    response.set("Access-Control-Allow-Origin", "*");
+    let Lenguaje = request.params.Lenguaje;
+    MongoClient.connect("mongodb://localhost:27017/", (err, client) => {
+      if (err) throw err;
+  
+      let database = client.db("Proyecto-Final-Releevant");
+  
+      database
+        .collection("post")
+        .find({ "Lenguaje": Lenguaje })
+        .toArray((err, results) => {
+          if (err) throw err;
+  
+          results.forEach((value) => {
+            console.log(value.Lenguaje);
+          });
+          response.json(results);
+        });
+    });
+  });
 
 
 
